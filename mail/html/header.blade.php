c03d3e9ab4a5d3e44aecda8d184b07c79630ff1b
Adriel Renato <adriel.renato@asteroidetecnologia.com.br>
2020-07-14 14:47:01 -0300

    feat: Criacao do layout do email

---
+++ mail/html/header.blade.php
@@ -0,0 +1,10 @@
+<tr>
+    <td class="header">
+        <img src="{{ env('LOGO_EMAIL') }}" alt="Logo Asteroide Pass" class="logo">
+    </td>
+</tr>
+<tr>
+    <td class="subject">
+        {{ $subject }}
+    </td>
+</tr>
